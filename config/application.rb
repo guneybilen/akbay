6236cbc2fff1460817957da0a981d725d9bf2633
Guney Bilen <guneybilen@yahoo.com>
2014-09-07 00:29:08 +0300

    asset precompile take 1

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Akbay
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
+    config.assets.css_compressor = :yui
+    config.assets.js_compressor = :yui
   end
 end
