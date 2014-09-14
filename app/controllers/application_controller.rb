426424c53b0ce3a5bd241cb361d14f1a78423c65
Guney Bilen <guneybilen@yahoo.com>
2014-09-14 21:33:33 +0300

    fix a glitch and force page refresh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :set_cache_headers
 
   protect_from_forgery with: :exception
 
@@ -36,5 +37,9 @@ class ApplicationController < ActionController::Base
      end
      return nil
    end
+
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+  end
 
 end
